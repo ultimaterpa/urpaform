0db6c1f6946bfab7a1b1edf184fdd780da2699e8
lukas <lukas.naplava@stringdata.cz>
2021-09-14 11:56:04 +0200

    Added tests for checking type and value

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,21 @@
 import pytest
 
 pytest.MonkeyPatch().syspath_prepend("mock")
+
+from urpa import AppElement
+
+from urpaform.elements import CheckElement, ComboElement, EditElement, PasswordElement, RadioElement
+from urpaform.form import Form
+
+
+@pytest.fixture(name="form", scope="session")
+def form_fixture():
+    form = Form()
+    form.add(
+        (EditElement(AppElement()), "John Doe"),
+        (PasswordElement(AppElement()), "1234"),
+        (CheckElement(AppElement()), True),
+        (RadioElement(AppElement()), True),
+        (ComboElement(AppElement()), "Monday"),
+    )
+    return form
--- tests/test_urpaform.py
+++ tests/test_urpaform.py
@@ -1,11 +1,37 @@
-import urpaform
-from urpaform.elements import _FormElement
-import urpa
+"""Testy pro urpaform"""
+
+import pytest
+
+from urpaform.elements import CheckElement, ComboElement, EditElement, PasswordElement, RadioElement
 
 
-def test_equality():
-    assert 1 == 1
+@pytest.mark.parametrize(
+    "index, value",
+    [
+        (0, "John Doe"),
+        (1, "1234"),
+        (2, True),
+        (3, True),
+        (4, "Monday"),
+    ],
+)
+def test_field_values_in_form(form, index, value) -> None:
+    assert form.elements[index][1] == value
 
 
-def test_form_element():
-    element = _FormElement()
+@pytest.mark.parametrize(
+    "index_1, index_2, expected",
+    [
+        (0, 0, EditElement),
+        (0, 1, str),
+        (1, 0, PasswordElement),
+        (2, 0, CheckElement),
+        (2, 1, bool),
+        (3, 0, RadioElement),
+        (3, 1, bool),
+        (4, 0, ComboElement),
+        (4, 1, str),
+    ],
+)
+def test_types_in_form(form, index_1, index_2, expected) -> None:
+    assert isinstance(form.elements[index_1][index_2], expected)
